caef408d89edb7c5220993649e2f358a90628044
BlackPearlDev <david.friquet@zohomail.eu>
2022-11-24 10:39:30 +0100

    add template admin dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,37 +12,51 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
+        <form class="js-validation-signin" method="POST" action="{{ route('login') }}">
             @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="form-group">
+                <div class="input-group">
+                    <input type="email" class="form-control" id="email" name="email" :value="old('email')" required autofocus placeholder="Email">
+                    <div class="input-group-append">
+                        <span class="input-group-text">
+                            <i class="fa fa-user-circle"></i>
+                        </span>
+                    </div>
+                </div>
             </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <div class="form-group">
+                <div class="input-group">
+                    <input type="password" id="password" class="form-control" name="password" required autocomplete="current-password" placeholder="Password">
+                    <div class="input-group-append">
+                        <span class="input-group-text">
+                            <i class="fa fa-asterisk"></i>
+                        </span>
+                    </div>
+                </div>
             </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
+            <div class="form-group d-sm-flex justify-content-lg-between align-items-lg-center text-center text-lg-left">
+                <div class="block mt-4">
+                        <label for="remember_me" class="flex items-center">
+                            <x-jet-checkbox id="remember_me" name="remember" />
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        </label>
+                </div>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
+            <div class="form-group text-center">
+                <x-jet-button class="btn btn-hero-primary">
+                    <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Log In
                 </x-jet-button>
             </div>
+            <p class="mt-3 mb-0 d-lg-flex justify-content-lg-between">
+                @if (Route::has('password.request'))
+                <a class="btn btn-sm btn-light d-block d-lg-inline-block mb-1" href="{{ route('password.request') }}"> 
+                    <i class="fa fa-exclamation-triangle text-muted mr-1"></i> Forgot password
+                </a>
+                @endif
+                <a class="btn btn-sm btn-light d-block d-lg-inline-block mb-1" href="{{ route('register') }}">
+                    <i class="fa fa-plus text-muted mr-1"></i> New Account
+                </a>
+            </p>
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,8 +15,51 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+        <div id="page-container">
+            <main id="main-container">
+                <div class="bg-dark">
+                    <div class="row no-gutters justify-content-center bg-dark">
+                        <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
+
+                            <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
+                                <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-white">
+                                    @if (Route::has('login'))
+                                        @auth
+                                        <div class="mb-2 text-center">
+                                            <a class="link-fx font-w700 font-size-h1" href="{{ route('dashboard') }}">
+                                                <span class="text-dark">Guest</span><span class="text-primary">Lucky</span>
+                                            </a>
+                                        </div>
+                                        @else
+                                        <div class="mb-2 text-center">
+                                            <a class="link-fx font-w700 font-size-h1" href="{{ route('login') }}">
+                                                <span class="text-dark">Guest</span><span class="text-primary">Lucky</span>
+                                            </a>
+                                        </div>
+                                        @endauth
+                                    @endif
+                                    {{ $slot ?? ""}}
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     </body>
 </html>
+
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
